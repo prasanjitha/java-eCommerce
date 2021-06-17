1ea373d8e44a12406a9105795e89917553e64483
prasanjitha <graphicdis126@gmail.com>
2021-06-17 10:33:02 +0530

    created login and register form UI design

--- app/src/main/java/com/example/e_commerce/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -6,36 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-   private Button button;
-    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        button1 = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener(){
+        TextView btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openRegister();
+                startActivity(new Intent(MainActivity.this,register.class));
             }
         });
-        button1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openHome();
-            }
-        });
+
     }
-    public void openRegister(){
-        Intent intent=new Intent(this, register.class);
-       startActivity(intent);
-    }
-    public void openHome(){
-        Intent intent=new Intent(this, home.class);
-        startActivity(intent);
-    }
+
+
 }
--- app/src/main/java/com/example/e_commerce/register.java
+++ app/src/main/java/com/example/e_commerce/register.java
@@ -6,27 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class register extends AppCompatActivity {
-    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        button =(Button) findViewById(R.id.button);
-        button =(Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener(){
+        TextView btn = findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openLogin();
-
+                startActivity(new Intent(register.this, MainActivity.class));
             }
         });
     }
-    public void openLogin(){
-        Intent intent=new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
 }
